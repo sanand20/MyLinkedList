30a9d8ef42ac3ae6a29e46e2406efc516495151e
Shriya <>
2021-01-07 19:21:22 -0500

    set done

--- Node.java
+++ Node.java
@@ -6,8 +6,14 @@ private Node next,prev;
 public Node(String value){
  data = value;
   }
-public String getData() {
+public String get() {
   return data;
 }
+
+public void set(String data){
+  this.data = data;
+}
+
+
 //write get/set methods for all three instance variables.
 }
