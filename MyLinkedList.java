37e5e191cb3b8031902d2ec96b6aa6c33975d53e
Shriya <>
2021-01-07 22:15:54 -0500

    adds done

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyLinkedList{
  private int size;
  private Node start,end;
@@ -7,20 +8,47 @@ public class MyLinkedList{
    return size;
  }
  public boolean add(String value){
-   if(size == 0){
-     start = new Node(value);
-     end = new Node(value);
-   }
-   else{
-     end.setNext(new Node(value));
-     end = Node(value);
-     size++
-   }
-   return true;
- }
- public void add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
- //Any helper method that returns a Node object MUST BE PRIVATE!
+     add(size, value);
+     return true;
+ };
+ public void add(int index, String value){
+     if (index >= 0 && index <= size){
+         if (start != null){
+             if (index == size){
+                 Node temp = new Node(value);
+                 end.setNext(temp);
+                 temp.setPrev(end);
+                 end = temp;
+             }
+             else if (index == 0){
+                 Node temp = new Node(value);
+                 start.setPrev(temp);
+                 temp.setNext(start);
+                 start = temp;
+             }
+             else{
+               Node current = start;
+                 for (int i = 0; i < index; i++){
+                      current = current.getNext();
+                 }
+               Node temp = new Node(value);
+               temp.setPrev(current.getPrev());
+               temp.setNext(current);
+               current.getPrev().setNext(temp);
+               current.setPrev(temp);
+             }
+         }
+         else{
+             Node temp = new Node(value);
+             start = temp;
+             end = temp;
+
+
+         }
+         size += 1;
+     }else{
+         throw new IndexOutOfBoundsException();
+     }
+ };
+ 
 }
