2c5faf102305295507fab26a6af481d4bd09158e
Shriya <>
2021-01-07 19:17:12 -0500

    getdata done

--- Node.java
+++ Node.java
@@ -1,6 +1,13 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
- private String data;
- private Node next,prev;
- //write get/set methods for all three instance variables.
+
+
+private String data;
+private Node next,prev;
+public Node(String value){
+ data = value;
+  }
+public String getData() {
+  return data;
+}
+//write get/set methods for all three instance variables.
 }
